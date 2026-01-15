1f12b7e3dc7b2666e0bd09c35b267232b2bab30c
raushan <raushanc107@gmail.com>
2026-01-15 23:12:01 +0530

    fix: mobile status bar color consistency by initializing theme service

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeService } from './core/services/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
+  private themeService = inject(ThemeService);
   protected readonly title = signal('khatabook');
 }
